619c967dcc29e0de24890bf2d8eb8cd06196babd
ganiaaldi <mrgad98@gmail.com>
2021-07-13 08:03:03 +0700

    setting gradle projects, and intergrate to firebase crashlytics

--- app/src/main/java/com/gadidev/acar/MainActivity.kt
+++ app/src/main/java/com/gadidev/acar/MainActivity.kt
@@ -2,10 +2,16 @@ package com.gadidev.acar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gadidev.acar.databinding.ActivityMainBinding
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        FirebaseCrashlytics.getInstance()
     }
 }
