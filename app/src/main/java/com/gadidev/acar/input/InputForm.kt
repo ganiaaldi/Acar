cfef71b4aef844dc8a4d8362156e64c6c0803ded
ganiaaldi <mrgad98@gmail.com>
2021-07-26 18:36:34 +0700

    create ui input form

---
+++ app/src/main/java/com/gadidev/acar/input/InputForm.kt
@@ -0,0 +1,48 @@
+package com.gadidev.acar.input
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.gadidev.acar.R
+import com.gadidev.acar.databinding.FragmentInputFormBinding
+
+class InputForm : Fragment() {
+    private var _binding: FragmentInputFormBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentInputFormBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        nextBtn()
+    }
+
+    private fun nextBtn() {
+        binding.btnNext.setOnClickListener {
+            findNavController().navigate(R.id.resultFragment)
+        }
+    }
+
+}
